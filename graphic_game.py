fd6580cb730f9927710fcd9fd2d34224e2fada39
Talha1337 <talhajaved@hotmail.co.uk>
2026-02-20 11:57:28 +0000

    graphic game placed into class

--- graphic_game.py
+++ graphic_game.py
@@ -10,11 +10,10 @@ def coord_to_px(coords: tuple[int, int]) -> tuple[int, int]:
     return tuple([int((TILEWIDTH) * coord) for coord in coords])
 
 
-class ScrabbleSurface(pygame.Surface):
+class ScrabbleGraphicGame:
     def __init__(self):
         pygame.init()
-        pygame.display.set_mode((600, 800))
-        self.fill("green")
+        self.surf = pygame.display.set_mode((600, 800))
         self.bg_image = pygame.image.load("images/scrabble_board_img.jpg")
 
     def populate_with_coords(self):
@@ -22,23 +21,24 @@ class ScrabbleSurface(pygame.Surface):
             for j in range(15):
                 coord_value = (i, j)
                 px_value = coord_to_px(coord_value)
-                pygame.draw.circle(self, (255, 0, 0), px_value, 10)
+                pygame.draw.circle(self.surf, (255, 0, 0), px_value, 10)
 
     def add_bg(self):
-        self.blit(self.bg_image, (0, 0))
+        self.surf.fill("green")
+        self.surf.blit(self.bg_image, (0, 0))
 
     def add_tile(self, pos: tuple[int, int]):
         """
         Adding the white scrabble tile to the point.
         """
-        pygame.draw.rect(self, (255, 255, 255), (pos[0], pos[1], 40, 40))
+        pygame.draw.rect(self.surf, (255, 255, 255), (pos[0], pos[1], 40, 40))
 
     def add_letter(self, pos: tuple[int, int], letter: str):
 
         my_font = pygame.font.SysFont("Calibri", 36, bold=True)
         text_surface = my_font.render(letter, False, (0, 0, 0))
         letter_pos = (pos[0] + TILEWIDTH / 5, pos[1])
-        self.blit(text_surface, letter_pos)
+        self.surf.blit(text_surface, letter_pos)
 
     def add_scrabble_tile(self, pos: tuple[int, int], letter: str):
         self.add_tile(pos)
@@ -56,7 +56,7 @@ class ScrabbleSurface(pygame.Surface):
 
 
 # Setting game window dimensions
-game_display = ScrabbleSurface()
+game_display = ScrabbleGraphicGame()
 # bg_image = pygame.transform.scale(bg_image, (1, 1))
 # Main game loop
 
@@ -71,5 +71,5 @@ while running:
     game_display.populate_with_coords()
     game_display.add_tile(coord_to_px((0, 0)))
     game_display.add_letter(coord_to_px((0, 0)), "A")
-    pygame.display.flip()
+    pygame.display.update()
 pygame.quit()
